3d0ad9a79d1b42bb23e8a42d14c94db2281e8367
Sailesh Dahal <saileshbro@gmail.com>
2019-01-12 22:37:32 +0545

    completed

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ fetch(url).then((res)=>{
 }).then((data)=>{
    countries = data;
    buildSelect(data);
+    document.querySelector('.flag').src = countries[156].flag;
+    document.querySelector('.languages').innerHTML="";
+    for(let i=0;i<countries[156].languages.length;i++){
+        document.querySelector('.languages').innerHTML += "<li>"+countries[156].languages[i].name+"</li>";
+    }
+    document.querySelector('.area').innerHTML = countries[156].area+" sq. kms";
+    document.querySelector('.capital').innerHTML = countries[156].capital;
+    document.querySelector('.population').innerHTML = countries[156].population;
+    document.querySelector('.currency').innerHTML = countries[156].currencies[0].name;
+    document.querySelector('.country-list').options[157].selected="selected";
 });
 function buildSelect(d){
     for(let i=0;i<d.length;i++){
@@ -24,7 +34,7 @@ function loadInformation(e){
     for(let i=0;i<countries[index].languages.length;i++){
         document.querySelector('.languages').innerHTML += "<li>"+countries[index].languages[i].name+"</li>";
     }
-    document.querySelector('.area').innerHTML = countries[index].area;
+    document.querySelector('.area').innerHTML = countries[index].area+" sq. kms";
     document.querySelector('.capital').innerHTML = countries[index].capital;
     document.querySelector('.population').innerHTML = countries[index].population;
     document.querySelector('.currency').innerHTML = countries[index].currencies[0].name;
